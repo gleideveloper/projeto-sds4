de253210e11df34ed364ccf047582a9249165564
Gleides Vinente da Silva <gleidevelop@gmail.com>
2021-09-09 23:39:18 -0400

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import NavBar from "./components/navbar";
+import Footer from "./components/footer";
+import DataTable from "./components/datatable";
+
 function App() {
-  return (
-    <div>
-      <h1 className="text-primary"> Meu projeto Spring com React</h1>
-    </div>
-  );
+    return (
+        <>
+            <NavBar/>
+            <div className="container">
+                <h1 className="text-primary"> Meu projeto Spring com React</h1>
+                <DataTable/>
+            </div>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
