0e488e639990c7d3629c25928b03dc5483fe459e
Gleides Vinente da Silva <gleidevelop@gmail.com>
2021-09-10 00:12:14 -0400

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,30 @@
 import NavBar from "./components/navbar";
 import Footer from "./components/footer";
 import DataTable from "./components/datatable";
+import BarChart from "./components/barchart";
+import DonutChart from "./components/donutChart";
 
 function App() {
     return (
         <>
             <NavBar/>
             <div className="container">
-                <h1 className="text-primary"> Meu projeto Spring com React</h1>
+                <h1 className="text-primary py-3"> Dashboard de Vendas</h1>
+
+                <div className="row px-3">
+                    <div className="col-sm-6">
+                        <h5 className={"text-center text-secondary"}>Taxa de sucesso</h5>
+                        <BarChart/>
+                    </div>
+                    <div className={"col-sm-6"}>
+                        <h5>Todas as Vendas</h5>
+                        <DonutChart/>
+                    </div>
+
+                </div>
+                <div className={"py-3"}>
+                    <h2 className={"text-primary"}>Todas as Vendas</h2>
+                </div>
                 <DataTable/>
             </div>
             <Footer/>
